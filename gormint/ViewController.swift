e3c0f1eeb2bc009c7915b8e630e4dac5e8325ee8
Nilav Vaghasiya <nilav@Nilavs-Mac.local>
2017-02-12 15:28:23 +0530

    Initial Commit

---
+++ gormint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gormint
+//
+//  Created by Nilav Vaghasiya on 2/12/17.
+//  Copyright © 2017 zeolabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
